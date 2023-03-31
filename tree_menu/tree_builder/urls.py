dcdd0ab56bb3e034d38904085a96add591ef91f3
versuffer <versuffer@mail.ru>
2023-03-31 10:21:01 +0300

    Added initial functionality to the draw_menu view

--- tree_menu/tree_builder/urls.py
+++ tree_menu/tree_builder/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import start_page, draw_menus
+from .views import start_page, draw_menu
 
 urlpatterns = [
-    # path('', start_page),
-    # path('<str:node_id>/', draw_menus),
+    path('', start_page),
+    path('<str:menu_name>/<int:node_id>', draw_menu),
 ]
--- tree_menu/tree_builder/views.py
+++ tree_menu/tree_builder/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 from .models import Node
 
 
@@ -6,7 +8,15 @@ def start_page(request):
     pass
 
 
-def draw_menus(request, node_id: str):
-    queryset = Node.objects.all()
+def draw_menu(request, menu_name: str, node_id: str):
+    menu_nodes = Node.objects.filter(menu_name=menu_name)
+    head_node = menu_nodes.get(id=node_id)
+    path_nodes = []
+    while True:
+        path_nodes.append(head_node)
+        if head_node.parent:
+            head_node = head_node.parent
+        else:
+            break
 
-    pass
+    return HttpResponse(path_nodes)
