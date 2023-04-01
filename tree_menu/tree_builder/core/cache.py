229d2f4099a6a0a5ef9f77e2b032f43071776857
versuffer <versuffer@mail.ru>
2023-04-01 12:58:37 +0300

    Rewrited menu rendering

---
+++ tree_menu/tree_builder/core/cache.py
@@ -0,0 +1,11 @@
+from django.core.cache import cache
+from tree_builder.models import Node
+
+
+def cache_menu_query(menu_name):
+    queryset = cache.get(f'{menu_name}_queryset')
+    if queryset is None:
+        queryset = Node.objects.filter(menu_name=menu_name)
+        cache.set(f'{menu_name}_queryset', queryset, timeout=86400)
+
+    return queryset
--- tree_menu/tree_builder/urls.py
+++ tree_menu/tree_builder/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import draw_menu, start_page
+from .views import draw_menu, start_page, draw
 
 urlpatterns = [
     path('', start_page, name='start'),
-    path('<str:menu_name>/<str:url_name>', draw_menu, name='menu'),
+    path('<str:menu_name>/<str:url_name>', draw, name='menu'),
 ]
